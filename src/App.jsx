104d0a1b8584e9b8afc467baae8d1ce48e864bd3
Dadu Khan <photoshopt65@gmail.com>
2025-03-14 16:24:40 +0500

    app finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
 
 const App = () => {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  })
+
+  const inputCheck = userInput.duration >= 1;
+
+  const handleChange = (inputIdentifier, newInput) => {
+    setUserInput((previousValue) => {
+      return {
+        ...previousValue,
+        [inputIdentifier]: +newInput
+      }
+    })
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputCheck && <p className='center'>Please enter a duration greater than zero.</p>}
+      {inputCheck && <Results userInput={userInput} />}
+    </>
   )
 }
 
